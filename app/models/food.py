6f773d0d7efe028c91121601e05b9e8d50ceea3d
hexdien <hexdien@proton.me>
2026-03-31 03:42:02 -0300

    food database class

---
+++ app/models/food.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Numeric
+from app.database.db import Base
+
+class Food(Base):
+    __tablename__ = "food"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+
+    calories = Column(Numeric, nullable=False)
+    carbs = Column(Numeric, nullable=False)
+    protein = Column(Numeric, nullable=False)
+    lipids = Column(Numeric, nullable=False)
