9a421dfa2389431f8f248e86937486442f770403
hexdien <hexdien@proton.me>
2026-04-28 02:43:31 -0300

    adicionando exceção de rollback e banco sqlite para testes

--- app/database/db.py
+++ app/database/db.py
@@ -2,7 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config.config import Config
 
-engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+engine_kwargs = {}
+
+if Config.SQLALCHEMY_DATABASE_URI.startswith("sqlite"):
+    engine_kwargs["connect_args"] = {"check_same_thread": False}
+
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, **engine_kwargs)
 
 SessionLocal = sessionmaker(bind=engine)
 
